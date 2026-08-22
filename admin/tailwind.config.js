d4bcf3303c2460075af7c2f59afb1193b9a38e2f
DharmTrivedi <dharmtrivedi2605@gmail.com>
2026-08-22 15:33:59 +0530

    style(admin): switch type pairing to Sora + Manrope

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -4,8 +4,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        display: ['Poppins', 'sans-serif'],
-        body:    ['Inter', 'sans-serif'],
+        display: ['Sora', 'sans-serif'],
+        body:    ['Manrope', 'sans-serif'],
       },
       colors: {
         cream:  { DEFAULT:'#F7F6F2', 100:'#F7F6F2', 200:'#EFEDE7' },
